2a82f6b8acc5630714ecbf3ce7d6a3a7a919e8e3
garvit2126 <garvit2126@users.noreply.github.com>
2021-04-08 18:41:30 +0530

    Fix maybe

--- server.js
+++ server.js
@@ -26,19 +26,21 @@ const adminLoginRoutes = require('./src/Routes/admin/admin')
 const adminHomeRoutes = require('./src/Routes/admin/Home/home')
 const adminLoggerRoutes = require('./src/logger/getLogger')
 
-var allowedOrigins = ['http://localhost:3000'];
+var allowedOrigins = ['http://localhost:3000','https://admin-gtbpi.netlify.app'];
 
 app.use(cors({
-    origin: function(origin, callback){    // allow requests with no origin
-        if(!origin) return callback(null, true);    
-        if(allowedOrigins.indexOf(origin) === -1){
-            var msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }       
-        return callback(null, true);
-},
-credentials: true
-}));
+    origin: function(origin, callback){    // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);    
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }    return callback(null, true);
+    },
+    credentials: true
+  }));
+
 
 app.disable('x-powered-by')
 app.use(express.json())
