29850900863b656192b044640d688c88df334dc5
garvit2126 <garvit2126@users.noreply.github.com>
2021-04-08 18:10:24 +0530

    This may fix cookie error

--- src/Routes/studentRoutes/studentLogin.js
+++ src/Routes/studentRoutes/studentLogin.js
@@ -17,7 +17,7 @@ router.post('/login',
                         expires: new Date(Date.now() + 10800000),
                         secure: true,
                         path: "/",
-                        httpOnly: false
+                        httpOnly: true
                     })
                     res.status(200).send({isSuccess: true})
                 }catch(e){
